b4b5b1b2cb74ef43898b73019b09b3f5161a2e34
jonathan <jonathan@borderux.com>
2025-06-27 17:42:29 -0700

    add dev mode for figma plugin release

--- apps/figma-plugin/scripts/zip.mjs
+++ apps/figma-plugin/scripts/zip.mjs
@@ -9,18 +9,18 @@ import { fileURLToPath } from 'node:url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const parentDir = resolve(__dirname, '..');
-const releasesDir = path.join(parentDir, 'releases');
+const releasesDir = path.join(parentDir, 'dev-releases');
 
 // create a file to stream archive data to.
 if (!fs.existsSync(releasesDir)) {
   fs.mkdirSync(releasesDir);
 } else {
   // clear the releases directory
-  fs.rmdirSync(releasesDir, { recursive: true });
+  fs.rmSync(releasesDir, { recursive: true });
   fs.mkdirSync(releasesDir);
 }
 
-const output = fs.createWriteStream(parentDir + `/releases/recursica-plugin.zip`);
+const output = fs.createWriteStream(parentDir + `/dev-releases/recursica-plugin.zip`);
 const archive = archiver('zip', {
   zlib: { level: 9 }, // Sets the compression level.
 });
@@ -28,10 +28,10 @@ const archive = archiver('zip', {
 archive.pipe(output);
 
 // append files
-archive.file('manifest.json', { name: 'manifest.json' });
+archive.file('manifest.dev.json', { name: 'manifest.json' });
 archive.file('PLUGIN.md', { name: 'README.md' });
 
 // append files from a sub-directory
-archive.directory('dist/', 'dist');
+archive.directory('dist-dev/', 'dist');
 // finalize the archive
 archive.finalize();
