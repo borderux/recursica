bf5d5d1083abf7a9ac9ff37b0f1b0c7a5634ecc4
jonathan <jonathan@borderux.com>
2025-07-17 16:28:03 -0700

    do not re sync if the variable is the same

--- apps/figma-plugin/src/plugin/syncTokens.ts
+++ apps/figma-plugin/src/plugin/syncTokens.ts
@@ -1,5 +1,11 @@
 export async function syncTokens() {
   const libraries = await figma.teamLibrary.getAvailableLibraryVariableCollectionsAsync();
+  if (libraries.length === 0) {
+    figma.notify('No libraries connected to this project');
+    return figma.ui.postMessage({
+      type: 'SYNC_TOKENS_COMPLETE',
+    });
+  }
 
   // Get all library variables in parallel
   const libraryVariablesPromises = libraries.map(async (library) => {
@@ -17,34 +23,33 @@ export async function syncTokens() {
   const localCollections = await figma.variables.getLocalVariableCollectionsAsync();
 
   // Process local collections in parallel
-  const localCollectionPromises = localCollections
-    .filter((collection) => collection.name !== 'ID variables')
-    .map(async (collection) => {
-      const variablePromises = collection.variableIds.map(async (varId) => {
-        const variable = await figma.variables.getVariableByIdAsync(varId);
-        if (!variable) return null;
+  const localCollectionPromises = localCollections.map(async (collection) => {
+    const variablePromises = collection.variableIds.map(async (varId) => {
+      const variable = await figma.variables.getVariableByIdAsync(varId);
+      if (!variable) return null;
 
-        const modePromises = Object.entries(variable.valuesByMode).map(async ([key, modeVal]) => {
-          if (typeof modeVal === 'object' && modeVal !== null && 'type' in modeVal) {
-            const refVar = await figma.variables.getVariableByIdAsync(modeVal.id);
-            if (refVar) {
-              const newSyncVar = libraryVariablesMap.get(refVar.name);
-              if (newSyncVar) {
-                const newRefVal: VariableValue = {
-                  type: 'VARIABLE_ALIAS',
-                  id: newSyncVar.id,
-                };
-                variable.setValueForMode(key, newRefVal);
-              }
+      const modePromises = Object.entries(variable.valuesByMode).map(async ([key, modeVal]) => {
+        if (typeof modeVal === 'object' && modeVal !== null && 'type' in modeVal) {
+          const refVar = await figma.variables.getVariableByIdAsync(modeVal.id);
+          if (refVar) {
+            const newSyncVar = libraryVariablesMap.get(refVar.name);
+            if (refVar.remote && newSyncVar && newSyncVar.key !== refVar.key) {
+              console.log(`syncing ${refVar.name} to ${newSyncVar.name}`);
+              const newRefVal: VariableValue = {
+                type: 'VARIABLE_ALIAS',
+                id: newSyncVar.id,
+              };
+              variable.setValueForMode(key, newRefVal);
             }
           }
-        });
-        await Promise.all(modePromises);
-        return variable;
+        }
       });
-
-      return Promise.all(variablePromises);
+      await Promise.all(modePromises);
+      return variable;
     });
+
+    return Promise.all(variablePromises);
+  });
 
   await Promise.all(localCollectionPromises);
   figma.ui.postMessage({
