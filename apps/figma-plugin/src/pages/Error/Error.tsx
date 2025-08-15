7be4b7b3ef5c236ad44348f8efc6c67c34755ac5
jonathan <jonathan@borderux.com>
2025-08-15 16:12:48 -0700

    Refactor Error component in Figma plugin to use new icons and simplify error messaging
    - Replaced error handling logic with a static message and added a frown icon for visual feedback.
    - Updated layout and styling for better alignment and user experience.
    - Added new 'face_frown' icons to the icon library for use in the Error component.

--- apps/figma-plugin/src/pages/Error/Error.tsx
+++ apps/figma-plugin/src/pages/Error/Error.tsx
@@ -1,50 +1,19 @@
-import { Flex, Typography, Button } from '@recursica/ui-kit-mantine';
-import { NavLink } from 'react-router';
-import { useFigma } from '../../hooks/useFigma';
+import { Flex, Typography, Icon } from '@recursica/ui-kit-mantine';
 import { Layout } from '../../components';
 
 export function Error() {
-  const { error } = useFigma();
-
-  const getErrorMessage = () => {
-    switch (error) {
-      case 'NO_TOKENS_FOUND':
-        return 'There are no libraries added yet, make sure to connect them before initializing the repo.';
-      case 'NO_TOKENS_OR_THEMES_FOUND':
-        return 'It seems like themes or tokens library are missing, make sure you add them and connect them before initializing the repo.';
-      default:
-        return 'An unexpected error occurred. Please try again.';
-    }
-  };
-
   return (
-    <Layout
-      footer={
-        <Button
-          component={NavLink}
-          to='https://www.recursica.com/community'
-          target='_blank'
-          rel='noopener noreferrer'
-          label='Learn more'
-        />
-      }
-    >
-      <Flex direction='column' gap={16}>
-        <Typography
-          variant='body-1/normal'
-          textAlign='center'
-          color='layers/layer-0/elements/text/color'
-        >
-          {getErrorMessage()}
-        </Typography>
+    <Layout>
+      <Flex direction='column' gap={'size/spacer/1-5x'} align='center'>
+        <Icon name='face_frown_outline' size={32} />
 
         <Typography
-          variant='body-2/normal'
+          variant='h6'
           textAlign='center'
-          color='layers/layer-0/elements/text/color'
+          color='layers/layer-1/elements/text/color'
           opacity={0.84}
         >
-          Already added it? Try closing and opening your file.
+          You’re in the wrong file
         </Typography>
       </Flex>
     </Layout>
