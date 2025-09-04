c7f4eec6c29403b52a026e6e728e0f11109a3d2a
Matt Massey <matt@borderux.com>
2025-09-04 13:11:58 -0700

    Removed DELETE from options

--- packages/ui-kit-mantine/.storybook/main.ts
+++ packages/ui-kit-mantine/.storybook/main.ts
@@ -38,7 +38,7 @@ const config: StorybookConfig = {
           res.setHeader("Access-Control-Allow-Origin", "*");
           res.setHeader(
             "Access-Control-Allow-Methods",
-            "GET, POST, PUT, DELETE, OPTIONS",
+            "GET, POST, PUT, OPTIONS",
           );
           res.setHeader(
             "Access-Control-Allow-Headers",
