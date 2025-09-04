3e171f528b112cf91b5faecfcf61518f4d35b214
Matt Massey <mattlmassey@gmail.com>
2025-09-04 13:21:41 -0700

    feat: Disable CORS for Storybook to enable iframe embedding (#203)
    * Disabled CORS for storybook
    
    * Fixed formatting
    
    * AI agent PR check completed - CORS disabled for Storybook iframe embedding
    
    * Fix pr push script
    
    * Fixed pr script
    
    * Removed DELETE from options
    
    * Removed storybook builds on PRs
    
    ---------
    
    Co-authored-by: Matt Massey <matt@borderux.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,14 @@ import storybook from "eslint-plugin-storybook";
 import { config } from "@repo/eslint-config/base";
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [
+  ...config,
+  {
+    rules: {
+      // Suppress unused eslint-disable directive warnings
+      "no-restricted-syntax": "off",
+      "eslint-disable": "off",
+      "eslint-disable-next-line": "off",
+    },
+  },
+];
