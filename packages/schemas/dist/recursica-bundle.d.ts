8df7510e057509a27a55926f035826d41310c6b7
jonathan <jonathan@borderux.com>
2025-09-05 09:43:03 -0700

    revert changes

--- packages/schemas/dist/recursica-bundle.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * WARNING: This file is auto-generated from a JSON schema. Do not edit directly.
- */
-
-export interface RecursicaBundle {
-  [k: string]: unknown;
-}
--- packages/schemas/dist/recursica-icons.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * WARNING: This file is auto-generated from a JSON schema. Do not edit directly.
- */
-
-export interface RecursicaIcons {
-  [k: string]: unknown;
-}
