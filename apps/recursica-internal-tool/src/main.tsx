c1fbf0cf2c47746e0d84a4c73797e242f5f476ab
jonathan <jonathan@borderux.com>
2025-09-08 09:10:19 -0700

    refactor: rename internal tool package and remove console.log override utility
    - Change package name from "recursica-internal-tool" to "@recursica/internal-tool"
    - Remove console.log override initialization from main.tsx
    - Delete console override utility file to streamline codebase

--- apps/recursica-internal-tool/src/main.tsx
+++ apps/recursica-internal-tool/src/main.tsx
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { initializeConsoleOverride } from "./utils/consoleOverride";
-
-// Initialize console.log override to send logs to GTM
-initializeConsoleOverride();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
