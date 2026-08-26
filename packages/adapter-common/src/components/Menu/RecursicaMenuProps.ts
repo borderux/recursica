0179858560d4ac6ce5b8f73043228a4998c6d5f9
Matt Massey <matt@borderux.com>
2026-08-26 09:23:02 -0700

    Added maxHeight to menus

--- packages/adapter-common/src/components/Menu/RecursicaMenuProps.ts
+++ packages/adapter-common/src/components/Menu/RecursicaMenuProps.ts
@@ -1,5 +1,13 @@
+import React from "react";
+
 /**
  * Props for the Recursica Menu component.
  */
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface RecursicaMenuProps {}
+export interface RecursicaMenuProps {
+  /**
+   * Overrides the dropdown's token-driven max-height with an explicit pixel (or other CSS
+   * length) value. Escape hatch for callers that need an arbitrary menu height the design
+   * tokens don't express; leave unset to use the token default.
+   */
+  maxHeight?: React.CSSProperties["maxHeight"];
+}
